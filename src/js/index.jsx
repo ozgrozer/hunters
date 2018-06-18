bcaf69c418a24774189ab2ce4b6c6741e7341ec2
Ozgur <ozgr@live.com>
2018-06-18 16:50:54 +0300

    Add Bootstrap.

---
+++ src/js/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Popover, PopoverHeader, PopoverBody } from 'reactstrap'
+
+import './../css/style.scss'
+
+const ajax = (opts) => {
+  const request = new window.XMLHttpRequest()
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      opts.success(JSON.parse(request.responseText))
+    }
+  }
+  request.open('get', opts.url, true)
+  request.send()
+}
+
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      posts: {},
+      popoverOpen: {}
+    }
+  }
+
+  componentDidMount () {
+    ajax({
+      url: './hunters/posts.json',
+      success: (hunters) => {
+        this.setState({ posts: hunters })
+      }
+    })
+  }
+
+  toggle (key) {
+    const popoverOpen = this.state.popoverOpen
+    popoverOpen[key] = !this.state.popoverOpen[key]
+
+    this.setState({
+      popoverOpen: popoverOpen
+    })
+  }
+
+  render () {
+    const hunters = Object.keys(this.state.posts).map((key) => {
+      const hunter = this.state.posts[key]
+
+      return (
+        <div key={key} className='col-2'>
+          <img src='https://gradientjoy.com/100' alt='' className='card-img-top' id={`popover${key}`} onClick={this.toggle.bind(this, key)} />
+
+          <Popover placement='bottom' isOpen={this.state.popoverOpen[key]} target={`popover${key}`} toggle={this.toggle.bind(this, key)}>
+            <PopoverHeader>{hunter.name}</PopoverHeader>
+            <PopoverBody>
+              Posts: {hunter.posts_count}
+            </PopoverBody>
+          </Popover>
+        </div>
+      )
+    })
+
+    return (
+      <div className='row'>
+        {hunters}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
